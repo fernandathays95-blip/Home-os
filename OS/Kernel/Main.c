b5a568a7dff2cac338092a57bf1346ee457e8388
fernandathays95-blip <fernandathays95@gmail.com>
2025-09-29 11:45:07 -0300

    Create Main.c

---
+++ OS/Kernel/Main.c
@@ -0,0 +1,5 @@
+// Arquivo: HomeOS/kernel/errors.h
+
+#define HOMEOS_ERR_ROOTFS_NOT_FOUND     1  // Erro 1: Falha ao encontrar a pasta principal do OS (HomeOS apagada)
+#define HOMEOS_ERR_BASE_CRITICAL_FAIL   2  // Erro 2: Falha crítica na base (Toolchain/LibC)
+// ... outros erros ...
